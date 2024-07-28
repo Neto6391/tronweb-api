a04100bec767103c65ff2386936f2c63c4cbf72f
Neto6391 <neto6391@gmail.com>
2024-07-28 18:09:48 -0300

    fix(fixes configuration file of tronweb imports)

--- src/config/tronweb.js
+++ src/config/tronweb.js
@@ -2,4 +2,12 @@ const TronWeb = require('tronweb');
 require('dotenv').config();
 
 const HttpProvider = TronWeb.providers.HttpProvider;
-const fullNode = new HttpProvider(process, env.FULL_NODE);
+const fullNode = new HttpProvider(process.env.FULL_NODE);
+const solidityNode = new HttpProvider(process.env.SOLIDITY_NODE);
+const eventServer = process.env.EVENT_SERVER;
+
+const privateKey = process.env.PRIVATE_KEY;
+
+const tronWeb = new TronWeb(fullNode, solidityNode, eventServer, privateKey);
+
+module.exports = tronWeb;
